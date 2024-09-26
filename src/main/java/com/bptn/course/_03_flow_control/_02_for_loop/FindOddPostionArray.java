6db7f92ed496821d5f0d4b5f10ea43b646bdf472
Samuel Gurara <samueljambo81@gmail.com>
2024-09-26 17:58:13 -0400

    Array and for-loop examples added

---
+++ src/main/java/com/bptn/course/_03_flow_control/_02_for_loop/FindOddPostionArray.java
@@ -0,0 +1,24 @@
+package com.bptn.course._03_flow_control._02_for_loop;
+
+public class FindOddPostionArray {
+
+	public static void main(String[] args) {
+		int[] numbers = { 10, 20, 30, 40, 50 };
+
+		System.out.println("At odd indexes the elements are: ");
+
+		// Approach 1
+		for (int i = 1; i < numbers.length; i += 2) {
+			System.out.println(numbers[i]);
+		}
+
+		// Approach 2
+		for (int i = 0; i < numbers.length; i++) {
+			if (i % 2 != 0) {
+				System.out.println(numbers[i]);
+			}
+		}
+
+	}
+
+}
--- src/main/java/com/bptn/course/_04_arrays/Array.java
+++ src/main/java/com/bptn/course/_04_arrays/Array.java
@@ -3,6 +3,41 @@ package com.bptn.course._04_arrays;
 public class Array {
 
 	public static void main(String[] args) {
+		// Declare and initialize an array with 6 varibles
+		int[] myArray = { 3, 5, 7, 9, 11, 13 };
+		System.out.println(myArray);
+		System.out.println("First elment: " + myArray[0]);
+
+		System.out.println("Secon element: " + myArray[1]);
+
+		// update the first element to 38
+		myArray[0] = 30;
+
+		System.out.println("First elment: " + myArray[0]);
+
+		// update the last element with only the properties
+
+		System.out.println("Update the last element: " + myArray[myArray.length - 1]);
+
+		// traditional for loop
+
+		// use for loop to print all the elements of the
+		for (int i = 0; i < myArray.length; i++) {
+			System.out.println(myArray[i]);
+		}
+
+		// print all elements without using for loop
+
+		// System.out.println(myArray.toString(myArray);
+
+		for (int i = myArray.length - 1; i >= 0; i--) {
+			System.out.println(myArray[i]);
+		}
+
+		// Emhanced for-loop (for-each-loop)
+		for (int element : myArray) {
+			System.out.println(element);
+		}
 
 	}
 }
